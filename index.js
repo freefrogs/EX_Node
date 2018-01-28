9fd1cbdd7ed328b014432a29f045c0602e9e864f
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-01-28 18:06:30 +0100

    Finish task 13.7

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log(process.env.OS);
-console.log(process.env.LANG);
-console.log(process.version);
+var fs = require('fs');
+
+fs.readdir('./', function(err, files) {
+	fs.writeFile('./list.txt', files, function(err) {
+		if (err) throw err;
+		console.log('Data has been saved!');
+		fs.readFile('./list.txt', 'utf-8', function(err, data) {
+			console.log(data);
+		});
+	});
+});
