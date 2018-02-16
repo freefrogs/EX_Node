512d658a71ccc360c666c3092664d35590728bd4
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-02-16 12:07:19 +0100

    Add server code

---
+++ modules/server.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var colors = require('colors');
+
+var handlers = require('./handlers');
+
+function start() {
+	function onRequest(request, response) {
+		console.log('The inquiry was received'.green);
+		console.log('Inquiry ' + request.url + ' received.');
+
+		response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+
+		switch (request.url) {
+			case '/':
+			case '/start':
+				handlers.welcome(request, response);
+				break;
+			case '/upload':
+				handlers.upload(request, response);
+				break;
+			case '/show':
+				handlers.show(request, response);
+				break;
+			default:
+				handlers.error(request, response);
+		}
+	}
+	http.createServer(onRequest).listen(9000);
+
+	console.log('Server started!'.green);
+}
+
+exports.start = start;
